455d88b1f4f1127b05c34deeaad7f535833d4144
Andarilz <isgrammar@gmail.com>
2023-06-29 20:37:52 +0400

    write validation and tests for validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -17,11 +17,42 @@ RSpec.describe Article, type: :model do
       article.title = ""
 
       expect(article).not_to be_valid
-      expect(article.errors[:title]).to eq("can't be blank")
+      expect(article.errors[:title]).to include("can't be blank")
+
+    end
+
+    it "invalid content" do
+
+      article.content = ""
+
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+
+    end
+
+    it "invalid slag" do
+
+      article.slag = ""
+
+      expect(article).not_to be_valid
+      expect(article.errors[:slag]).to include("can't be blank")
+
+    end
+
+    it "uniq slag" do
+
+      article_first = create(:article, title: "Test1", content: "Test1", slag: "Test1")
+
+      expect(article_first).to be_valid
+
+      article_second = build(:article, title: "Test1", content: "Test1", slag: "Test1")
+
+      expect(article_second).not_to be_valid
+
+      expect(article_second.errors[:slag]).to include('has already been taken')
 
     end
 
   end
-
 
 end
