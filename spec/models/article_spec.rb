de77df48672cd45b0f1502a674f2b3fd6e7ae7fc
Andarilz <isgrammar@gmail.com>
2023-06-29 19:28:04 +0400

    test validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:article) { build(:article) }
+
+  describe "#validation" do
+
+    it "validation test" do
+
+      expect(article).to be_valid
+
+    end
+
+    it "invalid title" do
+
+      article.title = ""
+
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to eq("can't be blank")
+
+    end
+
+  end
+
+
 end
