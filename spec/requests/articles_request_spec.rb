ab77e7870851fc5eb195d8a2d14ebcad691acc6f
Andarilz <isgrammar@gmail.com>
2023-07-01 21:52:46 +0400

    controller index tests

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
 
+  describe "#index" do
+
+    it "success" do
+
+      get "/articles"
+
+      expect(response).to have_http_status(:ok)
+
+    end
+
+    it "get json" do
+
+      article = create(:article)
+
+      get "/articles"
+
+      body = JSON.parse(response.body)
+
+      expect(body).to eq(
+        data: [
+                {
+                  id: article.id,
+                  type: "articles",
+                  attributes: {
+                    title: article.title,
+                    content: article.content,
+                    slag: article.slag
+                  }
+                }
+        ]
+      )
+
+    end
+
+  end
+
 end
