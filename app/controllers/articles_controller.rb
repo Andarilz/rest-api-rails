94adab8e28e7ee35141df6c33b4dccf882ae6157
Andarilz <isgrammar@gmail.com>
2023-07-01 18:12:03 +0400

    index show actions + tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,15 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+    render json: @articles, status: :ok
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    if @article
+      render json: @article, status: :ok
+    end
+  end
+
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe "/articles routing" do
 
         expect(get "/articles").to route_to("articles#index")
 
+      end
+
+    end
+
+    it "routes to articles#show" do
+
+      aggregate_failures do
+
+        expect(get "/articles/2").to route_to("articles#show", id: "2")
+
     end
 
   end
